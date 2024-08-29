f2fc7c6cade01d64f7b62b393a8c482a545ea54f
Thomas Rowlands <thomas.s.rowlands@gmail.com>
2024-08-29 12:45:12 +0100

    Fixed archive update's date logging output

--- FAIRClinicalWorkflow/Workflow.py
+++ FAIRClinicalWorkflow/Workflow.py
@@ -7,6 +7,7 @@ import tarfile
 import ftplib
 import re
 from datetime import datetime
+from os.path import exists
 
 import regex
 
@@ -43,6 +44,8 @@ def download_archive(ftp, file, local_dir):
         os.makedirs(local_dir)
 
     local_filepath = os.path.join(local_dir, file)
+    if not os.path.exists("Output"):
+        os.mkdir("Output")
     with open(local_filepath, "wb") as local_file:
         ftp.retrbinary(F"RETR {file}", local_file.write)
     logger.info(F"Downloaded: {file}")
@@ -83,13 +86,42 @@ def extract_archive(archive_path, output_path):
 
 
 def process_new_archive(new_archive_path):
+    # Extract archive to the same location
     output_path = new_archive_path.rstrip(".tar.gz")
     extract_archive(new_archive_path, output_path)
+
+    # process full text articles
     full_text_folder = os.path.join(output_path, "Full-texts")
     filter_articles(output_path, "case report")
-    get_supplementary_files(full_text_folder)
-    supplementary_file_paths = [dirpath for (dirpath, dirname, filename) in os.walk(full_text_folder)]
+
+    # process supplementary files
+    supplementary_output_path = F"{output_path}_supplementary"
+    gather_supplementary_files(full_text_folder, supplementary_output_path)
     execute_movie_removal(full_text_folder)
+
+
+def gather_supplementary_files(full_text_folder: str, supplementary_output_path: str):
+    dirs = [(dirpath, dirname, filename) for (dirpath, dirname, filename) in os.walk(full_text_folder) if
+            not dirname]
+    filepaths = []
+    for dir_list in dirs:
+        for file in dir_list[2]:
+            filepaths.append(os.path.join(dir_list[0], file))
+    for file in filepaths:
+        if file.endswith("_bioc.json") or file.endswith("_tables.json"):
+            continue
+        file_dir = os.path.join(supplementary_output_path, os.path.dirname(file))
+        if not exists(file_dir):
+            os.makedirs(file_dir)
+        try:
+            pmcid = regex.search(r"PMC[0-9]*_", file)[0][:-1]
+            result = process_supplementary_files([file], pmcid=pmcid)
+            if not result:
+                log_unprocessed_supplementary_file(file, "Could not extract text", folder)
+        except Exception as ex:
+            log_unprocessed_supplementary_file(file, F"An error occurred: {ex}")
+            if os.path.exists("temp_extracted_files"):
+                shutil.rmtree("temp_extracted_files")
 
 
 def update_existing_archive(new_archive_path):
@@ -106,18 +138,16 @@ def update_local_archive_versions(archive_name, date_modified, new_archive=False
     :param new_archive: True if the archive has not previously existed, False otherwise
     :return: 
     """
-    with open("file_versions.tsv", "rw", encoding="utf-8") as f_out:
+    with open("file_versions.tsv", "r+", encoding="utf-8") as f_out:
         old_content = [x.split("\t") for x in f_out.readlines()]
-        new_content = []
+        new_content = old_content
         if new_archive:
-            new_content = old_content
-            new_content.append([archive_name, date_modified])
+            new_content.append([archive_name, date_modified + "\n"])
         else:
-            for name, date in old_content:
-                if archive_name == archive_name:
-                    new_content.append([archive_name, date_modified])
-                else:
-                    new_content.append([name, date])
+            index = [x for (x, y) in new_content].index(archive_name)
+            new_content[index][1] = date_modified + "\n"
+        f_out.seek(0)
+        f_out.truncate()
         f_out.writelines(["\t".join(x) for x in new_content])
 
 
@@ -132,37 +162,40 @@ def check_pmc_bioc_updates():
     with ftplib.FTP(ftp_server) as ftp:
         ftp.login()
         files = list_archives_with_dates(ftp, ftp_directory)
-        for filename, date_modified in files:
-            archive_processed = False
-            # Filter out any files that are not json_ascii.tar.gz
-            if "_json_ascii.tar.gz" not in filename:
-                continue
-            date_modified = date_modified.strftime("%Y-%m-%d %H:%M:%S")
-            # Check for updates to current files or brand-new ones
-            for [current_file, current_file_date] in current_versions:
-                if filename == current_file:
-                    if current_file_date != date_modified:
-                        logger.info(F"Updating {filename}")
-                        # An update is found for the already stored archive
+    for filename, date_modified in files:
+        archive_processed = False
+        # Filter out any files that are not json_ascii.tar.gz
+        if "_json_ascii.tar.gz" not in filename:
+            continue
+        date_modified = date_modified.strftime("%Y-%m-%d %H:%M:%S")
+        # Check for updates to current files or brand-new ones
+        for [current_file, current_file_date] in current_versions:
+            if filename == current_file:
+                if current_file_date != date_modified:
+                    logger.info(F"Updating {filename}")
+                    # An update is found for the already stored archive
+                    with ftplib.FTP(ftp_server) as ftp:
+                        ftp.login()
+                        ftp.cwd(ftp_directory)
                         download_archive(ftp, filename, "Output")
-                        update_existing_archive(os.path.join("Output", filename))
-                        update_local_archive_versions(filename, date_modified)
-                        archive_processed = True
-                        updates = True
-                        break
-                    else:
-                        archive_processed = True
-                        # Archive is already up-to-date
-                        break
-            # File was either updated or already up-to-date
-            if archive_processed:
-                continue
-            # A new archive has been found for processing
-            logger.info(F"Downloading new archive {filename}")
-            # download_file(ftp, filename, "Output")
-            download_archive(ftp, filename, "Output")
-            process_new_archive(os.path.join("Output", filename))
-            updates = True
+                    update_existing_archive(os.path.join("Output", filename))
+                    update_local_archive_versions(filename, date_modified)
+                    archive_processed = True
+                    updates = True
+                    break
+                else:
+                    archive_processed = True
+                    # Archive is already up-to-date
+                    break
+        # File was either updated or already up-to-date
+        if archive_processed:
+            continue
+        # A new archive has been found for processing
+        logger.info(F"Downloading new archive {filename}")
+        # download_file(ftp, filename, "Output")
+        download_archive(ftp, filename, "Output")
+        process_new_archive(os.path.join("Output", filename))
+        updates = True
     return updates
 
 
@@ -172,43 +205,7 @@ def log_unprocessed_supplementary_file(file, reason, package):
 
 
 def run():
-    # Debug code for supplementary material processing
-    folders = ["030", "035", "040", "045", "050", "055", "060", "065", "070", "075", "080", "085", "090", "095",
-               "100", "105"]
-    files_processed = 0
-    successful = 0
-    failed = 0
-    # folders = ["105"]
-    for folder in folders:
-        full_text_folder = F"D:\\Projects\\FAIRClinical\\Supplementary Files\\PMC{folder}XXXXX_supplementary"
-
-        dirs = [(dirpath, dirname, filename) for (dirpath, dirname, filename) in os.walk(full_text_folder) if
-                not dirname]
-        filepaths = []
-        for dir_list in dirs:
-            for file in dir_list[2]:
-                filepaths.append(os.path.join(dir_list[0], file))
-        for file in filepaths:
-            if file.endswith("_bioc.json") or file.endswith("_tables.json"):
-                continue
-            try:
-                files_processed += 1
-                pmcid = regex.search(r"PMC[0-9]*_", file)[0][:-1]
-                result = process_supplementary_files([file], pmcid=pmcid)
-                if not result:
-                    failed += 1
-                    log_unprocessed_supplementary_file(file, "Could not extract text", folder)
-                else:
-                    successful += 1
-            except Exception as ex:
-                failed += 1
-                log_unprocessed_supplementary_file(file, F"An error occurred: {ex}")
-                if os.path.exists("temp_extracted_files"):
-                    shutil.rmtree("temp_extracted_files")
-
-    print(F"Files processed: {files_processed}\nSuccessful: {successful}\nFailed: {failed}")
-    sys.exit()
-
+    # gather_supplementary_files("Output\\PMC000XXXX_json_ascii", "Output\\PMC000XXXXX_json_ascii_supplementary")
     logger.info("Checking for new archive versions...")
     updates = check_pmc_bioc_updates()
     if updates:
