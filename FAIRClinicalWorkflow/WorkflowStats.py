824895b5cabd3a5eeb12ecd0aa23bc46faae5c59
Thomas Rowlands <thomas.s.rowlands@gmail.com>
2024-11-06 14:53:27 +0100

    Added some error handling for potential file reading errors

--- FAIRClinicalWorkflow/WorkflowStats.py
+++ FAIRClinicalWorkflow/WorkflowStats.py
@@ -1,13 +1,24 @@
 import argparse
+import sys
 import tarfile
+from pathlib import Path
 
 
 def count_supplementary_files():
     pass
 
-def count_full_text_articles(input_directory):
-    with tarfile.open(input_directory) as tar:
-        return len(tar.getmembers())
+def count_full_text_articles(input_archive_path):
+    if not Path(input_archive_path).exists():
+        sys.exit("Provided path does not exist")
+    elif Path(input_archive_path).is_dir():
+        sys.exit("Provided path is not an archive")
+    try:
+        with tarfile.open(input_archive_path) as tar:
+            return len(tar.getmembers())
+    except tarfile.ReadError:
+        sys.exit("Read error encountered while attempting to process the input archive file.")
+    except IOError:
+        sys.exit("IO Error encountered, please ensure the archive path is correct and uncorrupted.")
 
 def __main():
     arg_parser = argparse.ArgumentParser()
